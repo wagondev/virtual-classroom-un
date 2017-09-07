a9d85e8c01b86d0c95f26f3b7718af315f1b3162
caralopezromUN <caralopezrom@unal.edu.co>
2017-09-07 16:24:24 -0500

    Landing Page

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+      $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 125) { // check if user scrolled more than 50 from top of the browser window
+          $(".navbar-fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+          $(".navbar-fixed-top").css("margin-top", "0"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)          
+          $(".navbar-default").css("background-color", 'black')
+
+        } else {
+          $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+          $(".navbar-fixed-top").css("margin-top", "1rem"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)          
+          $("a").css("color", '#fff');
+
+        }
+      });
+    });
+
