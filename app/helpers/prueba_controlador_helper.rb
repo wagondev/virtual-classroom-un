f50d3240a2942195b6eb45181c04e9088c73929c
caguevarag <caguevarag@gmail.com>
2017-09-07 15:42:43 -0500

    implementado autenticacion con devise

---
+++ app/helpers/prueba_controlador_helper.rb
@@ -0,0 +1,2 @@
+module PruebaControladorHelper
+end
--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,2 +1,4 @@
 class Administrator < ApplicationRecord
+    validates :idRol, numericality: true, presence: true, uniqueness: true    
+    validates :name, presence: true, lengh: {minimu: 2}, uniqueness: true
 end
