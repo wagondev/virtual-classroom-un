804bea4bd1cd89c5d82cacaa858473e659c0bd92
Omar Beltran <ojbeltranr@unal.edu.co>
2017-09-07 19:58:18 +0000

    Agregar validaciones a los atributos de las entidades

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+    validates :id_group, numericality: true, presence: true, uniqueness: true  
+    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates  :number_member, numericality: true 
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,4 @@
 class Teacher < ApplicationRecord
+    validates :idStudent, numericality: true, presence: true, uniqueness: true
+    validates :name, presence: true
 end
