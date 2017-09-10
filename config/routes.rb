ac3561bdd9027b0cab3b52b81a559c597fe9f83d
Unknown <caralopezrom@unal.edu.co>
2017-09-10 18:53:38 -0500

    Pagina sign-in modificada

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  #get 'home/index'
+  get 'home/index'
 
   resources :workteams
   get 'pages/info'
-  root :to => 'home#index'
+  root to: 'home#index'
   #root to: redirect('Landing%20Page/index')
   resources :tests
   devise_for :users
