1c513f930d65caafa3a688f7c386bd8ae2765822
David Herrera <dacherreragu@unal.edu.co>
2017-09-11 01:38:50 +0000

    Commit arreglo errores sintácticos validadores

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,10 @@
 class Student < ApplicationRecord
     validates :idStudent, numericality: true, presence: true, uniqueness: true
     validates :name, presence: true
-    ##validates :birth_day  
-    ##validates :city
-    ##validates :ocupation
-    ##validates :career
-    ##validates :preferences
-    ##validates :about_me
+    #validates :birth_day  
+    #validates :city
+    #validates :ocupation
+    #validates :career
+    #validates :preferences
+    #validates :about_me
 end
