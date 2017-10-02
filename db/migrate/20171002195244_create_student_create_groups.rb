f61100825cd2e1a9d81ecbc9f95e0745eb1f5eeb
David Herrera <dacherreragu@unal.edu.co>
2017-10-02 20:11:55 +0000

    Modelo completo y controlador vacío (corregido)

--- db/migrate/20171002195244_create_student_create_groups.rb
+++ db/migrate/20171002195244_create_student_create_groups.rb
@@ -1,8 +1,8 @@
 class CreateStudentCreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :student_create_groups do |t|
-      t.string, :nombre
-      t.integer, :maxIntegrantes
+      t.string :nombre
+      t.integer :maxIntegrantes
       t.text :descripcion
 
       t.timestamps
