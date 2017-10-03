67194f84a48945ebd48b265f3e4f4f560238bda1
David Herrera <dacherreragu@unal.edu.co>
2017-10-03 19:46:12 +0000

    Problema NoMethod...

--- app/controllers/student_create_groups_controller.rb
+++ app/controllers/student_create_groups_controller.rb
@@ -1,4 +1,7 @@
 class StudentCreateGroupsController < ApplicationController
   def create_groups
+    @nombre = params[:nombre]
+    @maxIntegrantes = params[:maxIntegrantes]
+    @descripcion = params[:descripcion]
   end
 end
