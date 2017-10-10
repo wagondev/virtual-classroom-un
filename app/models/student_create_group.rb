f09d66959f66c3ba8b36a0a15a22ae7e923174ea
David Herrera <dacherreragu@unal.edu.co>
2017-10-10 20:16:09 +0000

    Validaciones para la creación de grupos

--- app/models/student_create_group.rb
+++ app/models/student_create_group.rb
@@ -1,4 +1,5 @@
 class StudentCreateGroup < ApplicationRecord
-    validates :nombre, presence: true, uniqueness: true
+    validates :nombre, presence: true, uniqueness: true, length: {minimum: 3}
     validates :maxIntegrantes, :numericality => {greater_than_or_equal_to: 1}, presence: true
+    validates :descripcion, length: {maximum: 150}
 end
