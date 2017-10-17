e541eb24c9f5086c1e221565cbb0ce4ae9130e1b
Omar Beltran <ojbeltranr@unal.edu.co>
2017-10-17 20:32:51 +0000

    Crear tabla carrera

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :careers
   resources :inscriptions
   resources :groups
   resources :teams
---
+++ test/system/careers_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CareersTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit careers_url
+  #
+  #   assert_selector "h1", text: "Career"
+  # end
+end
