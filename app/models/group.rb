bf199bb8597e2b243d6123b168f0798e05702281
David Herrera <dacherreragu@unal.edu.co>
2017-10-25 05:21:15 +0000

    Corrección de seeds.rb, quitar la unicidad de los nombres de grupo

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, length: {minimum: 3}
+    validates :name, presence: true, length: {minimum: 3}
     belongs_to :subject, foreign_key: "subject_id", class_name: "Subject"
     #has_many :Teams
     has_many :Inscriptions, dependent: :destroy
