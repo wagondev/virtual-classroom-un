2b117343dc330bd9800b7968e69a3b07bc822fb3
David Herrera <dacherreragu@unal.edu.co>
2017-10-27 14:07:46 +0000

    Solución problema conversión string to integer PostgreSQL

--- db/migrate/20171016001331_add_columns_to_users.rb
+++ db/migrate/20171016001331_add_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :name, :string
-    add_column :users, :identification, :integer
+    add_column :users, :identification, :string
     add_column :users, :student, :boolean
     add_column :users, :photo, :string
     add_column :users, :career, :string
--- db/migrate/20171017051117_change_identification_to_be_integer_in_users.rb
+++ db/migrate/20171017051117_change_identification_to_be_integer_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeIdentificationToBeIntegerInUsers < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :identification, :integer
+    change_column :users, :identification, 'integer USING CAST(identification AS integer)'
   end
 end
