6537ccbbfc7eb8c1b13dbe291ba42511551c788d
Arturo Lopez <caralopezrom@unal.edu.co>
2017-11-07 11:00:16 -0500

    Query en cmodel

--- app/models/team.rb
+++ app/models/team.rb
@@ -12,7 +12,7 @@ class Team < ApplicationRecord
     def default_values
         self.description ||= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo architecto cum neque eligendi tenetur, earum libero iste eum similique aliquam provident. Nulla commodi velit deserunt adipisci veritatis, rerum ratione molestias!"
     end
-    def self.teamAvailableOpen
-        Team.includes([{members: :user},group: :teams]).where.not(users:{id: current_scope}).where( groups: {id: 1}).order(created_at: :desc)
+    def self.teamAvailableOpen(user_id)
+        Team.includes([{members: :user},group: :teams]).where.not(users:{id: user_id}).where( groups: {id: 1}).order(created_at: :desc)
     end
 end
