1eebd2e304afe95ad91600751ac8bebcc0da068f
David Herrera <dacherreragu@unal.edu.co>
2017-11-08 20:01:41 +0000

    Validaciones mensaje

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :member
   
-  validates :title, presence: true, length: {minimum: 3}
-  validates :max_member, presence: true, :numericality => {greater_than_or_equal_to: 1}
+  validates :title, presence: true, length: {minimum: 3, maximum: 140}
   validates :body, length: {maximum: 1000}, presence: true
+  validates :address, length: {maximum: 100}
   
   mount_uploaders :document, FileUploader
   serialize :document, JSON # If you use SQLite, add this line.
