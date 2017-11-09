59976def76f0bffec96ac22a6acce68416279e89
David Herrera <dacherreragu@unal.edu.co>
2017-11-09 20:18:49 +0000

    Subir archivos al muro.  Falta visualizarlos en el mensaje

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :member, foreign_key: "member_id", class_name: "Member"
   
   validates :title, presence: true, length: {minimum: 3, maximum: 140}
-  validates :body, length: {maximum: 1000}, presence: true
+  validates :body, length: {maximum: 1000}
   validates :address, length: {maximum: 100}
   geocoded_by :address
   after_validation :geocode
